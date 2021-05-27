da715a5885521ac3c2fa4e7787295b72ec7c1c89
thisaramanohara <thisaramanohara@gmail.com>
2021-05-27 12:49:52 +0530

    Have to code to the START GAME button

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,6 +4,7 @@ import {View,Text,StyleSheet, TextInput, Button , TouchableWithoutFeedback, Keyb
 import Card from '../components/Card'
 import Colors from '../constants/colors'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 
 const StartGameScreen = props=>{
 
@@ -30,13 +31,21 @@ const StartGameScreen = props=>{
         setConfirmed(true)
         setSelectedNumber(chosenNumber)
         setEnteredValue('')
+        Keyboard.dismiss()
         
     }
 
     let confirmedOutput
 
     if(confirmed) {
-        confirmedOutput = <Text>Chosen Number : {selectedNumber}</Text>
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START GAME" />
+            </Card>
+        
+        )
     }
 
     return (
@@ -95,6 +104,10 @@ const styles = StyleSheet.create({
     input:{
         width:50,
         textAlign:'center'
+    },
+    summaryContainer: {
+        marginTop:20,
+        alignItems:'center'
     }
 })
 
